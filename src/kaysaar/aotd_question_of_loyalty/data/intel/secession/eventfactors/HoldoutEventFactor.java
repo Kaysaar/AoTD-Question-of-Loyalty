2c47229261fd406a84855c1d4fc92c9ef7a1b335
erdfern <git@mail.erdfern.com>
2025-08-27 21:20:13 +0200

    Fix Division by Zero in Secession Event Progress
    I've encountered an issue in my current save where the game crashes after I lose a market in the faction secession event. The crash occurs on the next `HoldoutEventFactor.getProgress` call after losing the market.
    
    The issue is that when the ratio of marketsStillOwned to getOriginalMarkets.size is < 1.0, the int cast will produce 0, at which point we divide `BASE_POINTS` by zero.
    
    This patch has fixed the issue in my save.

--- src/kaysaar/aotd_question_of_loyalty/data/intel/secession/eventfactors/HoldoutEventFactor.java
+++ src/kaysaar/aotd_question_of_loyalty/data/intel/secession/eventfactors/HoldoutEventFactor.java
@@ -41,9 +41,20 @@ public class HoldoutEventFactor extends BaseEventFactor {
 
     @Override
     public int getProgress(BaseEventIntel intel) {
-        if(AoTDSecessionManager.get().getOriginalMarkets().isEmpty()){
+        int originalMarketSize = AoTDSecessionManager.get().getOriginalMarkets().size();
+        int marketsStillOwned = AoTDSecessionManager.get().getMarketsStillOwned();
+
+        if (originalMarketSize == 0) {
             return 0;
         }
-        return BASE_POINTS/(int)((float)AoTDSecessionManager.get().getMarketsStillOwned()/(float)AoTDSecessionManager.get().getOriginalMarkets().size());
+
+        float ownershipRatio = (float) marketsStillOwned / (float) originalMarketSize;
+        int denominator = (int) ownershipRatio;
+
+        if (denominator == 0) {
+            return 0;
+        }
+
+        return BASE_POINTS / denominator;
     }
 }
